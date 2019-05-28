a148f743cd3b07bbf571da01d43ca81628f5dad6
Jia Yu <Jia.Y@clubware.co.nz>
2019-05-29 10:26:37 +1200

    add step select shops

--- cypress/integration/cashconverters/firstTest.js
+++ cypress/integration/cashconverters/firstTest.js
@@ -3,11 +3,13 @@
 context('example test', () => {
     beforeEach(() => {
       cy.login(Cypress.env('username'), Cypress.env('password'));
-    //   cy.get('.navbar-nav').contains('Commands').click()
-    //   cy.get('.dropdown-menu').contains('Navigation').click()
     })
   
     it('first test case', () => {
         cy.get('h3').contains('Select Store').should("visible");
+        cy.get('button[data-id]').click();
+        cy.get('.dropdown-menu[aria-expanded]').contains('CC Birkenhead').click();
+        cy.get('form').submit();
+        cy.get('#login').contains('CC Birkenhead').should("visible");
     })
   })
