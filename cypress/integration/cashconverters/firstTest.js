c6c69956486a6e8244f5c61770dfb4446fa44b84
Jia Yu <Jia.Y@clubware.co.nz>
2019-05-29 15:35:53 +1200

    add data driven

--- cypress/integration/cashconverters/firstTest.js
+++ cypress/integration/cashconverters/firstTest.js
@@ -1,15 +1,22 @@
 /// <reference types="Cypress" />
 
 context('example test', () => {
+    let testData;
+    before(() => {
+        // cy.fixture("testData").as("testData");
+    });
     beforeEach(() => {
-      cy.login(Cypress.env('username'), Cypress.env('password'));
+        cy.fixture('testData').then(c => {testData = c});
+        cy.login(Cypress.env('username'), Cypress.env('password'));
     })
-  
-    it('first test case', () => {
-        cy.get('h3').contains('Select Store').should("visible");
-        cy.get('button[data-id]').click();
-        cy.get('.dropdown-menu[aria-expanded]').contains('CC Birkenhead').click();
-        cy.get('form').submit();
-        cy.get('#login').contains('CC Birkenhead').should("visible");
+    it(`test case`, () => {
+        cy.wrap(testData).each(td => {
+            cy.get('h3').contains('Select Store').should("visible");
+            cy.get('button[data-id]').click();
+            cy.get('.dropdown-menu[aria-expanded]').contains(td.store).click();
+            cy.get('form').submit();
+            cy.get('#login').contains(td.store).should("visible");
+        })
     })
-  })
+    
+})
